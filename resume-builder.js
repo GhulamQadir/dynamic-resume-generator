18c8cd06ff8b7d9a2304d9075d78e54ee312ffa2
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2024-09-08 18:02:24 +0500

    done

--- resume-builder.js
+++ resume-builder.js
@@ -1,21 +1,24 @@
 var userName = document.getElementById("name");
 var email = document.getElementById("email");
-var jobTitle = document.getElementById("email");
+var jobTitle = document.getElementById("jobTitle");
 var profileBio = document.getElementById("profileBio");
 var phoneNum = document.getElementById("phoneNum");
 var skill = document.getElementById("skillsInput");
 var education = document.getElementById("educationInput");
 var experience = document.getElementById("experienceInput");
+var language = document.getElementById("languageInput");
 var resumeDiv = document.getElementById("resumeDiv");
 var alertDiv = document.getElementById("alert");
 var errorMessage = document.getElementById("error_message");
 var educationList = document.getElementById("educationList");
 var skillsList = document.getElementById("skillsList");
 var experienceList = document.getElementById("experienceList");
+var languageList = document.getElementById("languageList");
 var selectedFile;
 var educationArray = [];
 var skillsArray = [];
 var experienceArray = [];
+var languageArray = [];
 var closeErrorAlert = function () {
     alertDiv.style.opacity = "0";
     setTimeout(function () {
@@ -26,7 +29,6 @@ var closeErrorAlert = function () {
 var addEducation = function () {
     if (education.value.trim().length < 1) {
         education.focus();
-        return;
     }
     else {
         educationArray.unshift(education.value);
@@ -37,7 +39,6 @@ var addEducation = function () {
 var addSkill = function () {
     if (skill.value.trim().length < 1) {
         skill.focus();
-        return;
     }
     else {
         skillsArray.unshift(skill.value);
@@ -48,7 +49,6 @@ var addSkill = function () {
 var addExperience = function () {
     if (experience.value.trim().length < 1) {
         experience.focus();
-        return;
     }
     else {
         experienceArray.unshift(experience.value);
@@ -56,39 +56,67 @@ var addExperience = function () {
         experience.value = "";
     }
 };
+var addLanguage = function () {
+    if (language.value.trim().length < 1) {
+        language.focus();
+    }
+    else {
+        languageArray.unshift(language.value);
+        languageList.innerHTML += "<li>".concat(language.value, "</li>");
+        language.value = "";
+    }
+};
 var createResume = function (event) {
     event.preventDefault();
     if (userName.value.trim().length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your name";
+        userName.focus();
     }
     else if (jobTitle.value.trim().length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your job title";
+        jobTitle.focus();
     }
     else if (email.value.trim().length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your email";
+        email.focus();
     }
     else if (phoneNum.value.trim().length < 0) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your phone number";
+        phoneNum.focus();
     }
     else if (profileBio.value.trim().length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your profile bio";
+        profileBio.focus();
     }
     else if (educationArray.length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please add atleast your last education";
+        education.focus();
     }
     else if (experienceArray.length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please enter your last experience";
+        experience.focus();
     }
     else if (skillsArray.length < 1) {
         alertDiv.style.display = "block";
         errorMessage.innerText = "Please add atleast one skill";
+        skill.focus();
+    }
+    else if (languageArray.length < 1) {
+        alertDiv.style.display = "block";
+        errorMessage.innerText = "Please add atleast one language";
+        language.focus();
+    }
+    else if (!selectedFile) {
+        alertDiv.style.display = "block";
+        errorMessage.innerText = "Please upload your profile picture";
+        language.focus();
     }
     else {
         var resume = {
@@ -100,14 +128,16 @@ var createResume = function (event) {
             skills: skillsArray,
             education: educationArray,
             experience: experienceArray,
+            languages: languageArray,
             profileImg: selectedFile,
         };
         localStorage.setItem("resume", JSON.stringify(resume));
-        resumeDiv.innerHTML = "<div>\n      <h1>Your name: ".concat(userName.value, "</h1>\n      <h1>Your email: ").concat(email.value, "</h1>\n      <h1>Your phone number: ").concat(phoneNum.value, "</h1>\n      <img src=\"").concat(selectedFile, "\" height=\"100\" width=\"100\" />\n      </div>");
-        userName.value = "";
-        email.value = "";
-        phoneNum.value = "";
+        var generatedLink = "https://dynamic-resume-generator-fg4kq6y7p-ghulamqadir1s-projects.vercel.app/".concat(userName.value);
+        resumeDiv.innerHTML = "<div id=\"myResumeDiv\">\n        <div class=\"headerDiv\">\n            <div class=\"leftHandHeader\">\n                <div>\n                    <p class=\"name\">".concat(userName.value, "</p>\n                    <p class=\"jobTitle\">").concat(jobTitle.value, "</p>\n                </div>\n                <img class=\"profileImg\" src=\"").concat(selectedFile, "\" />\n            </div>\n            <div class=\"rightHandHeader\">\n                <p class=\"email\">Email: ").concat(email.value, "</p>\n                <p class=\"phoneNo\">Phone No: ").concat(phoneNum.value, "</p>\n            </div>\n        </div>\n\n           <div class=\"mainContentDiv\">\n            <div class=\"leftHandMainDiv\">\n                <div class=\"aboutMeDiv mainDivs\">\n                    <p class=\"mainDivHeadings\">About Me</p>\n                    <p class=\"aboutMeContent\">").concat(profileBio.value, "</p>\n                </div>\n                <div class=\"educationDiv mainDivs\">\n                    <p class=\"mainDivHeadings\">Education</p>\n                    <ul id=\"educationList\">\n            ").concat(educationArray.map(function (educationItem) { return "<li>".concat(educationItem, "</li>"); }), "\n                    </ul>\n                </div>\n                <div class=\"experienceDiv mainDivs\">\n                    <p class=\"mainDivHeadings\">Experience</p>\n                  \n                    <ul id=\"experienceList\">\n                            ").concat(experienceArray.map(function (expItem) { return "<li>".concat(expItem, "</li>"); }), "\n                    </ul>\n                </div>\n            </div>\n\n            <div class=\"rightHandMainDiv\">\n                <div class=\"experienceDiv mainDivs\">\n                    <p class=\"mainDivHeadings\">Skills</p>\n                    <ul id=\"skillsList\">\n                             ").concat(skillsArray.map(function (skillItem) { return "<li>".concat(skillItem, "</li>"); }), "\n                    </ul>\n                </div>\n                <div class=\"languageDiv\">\n                    <p class=\"mainDivHeadings mainDivs\">Languages</p>\n                    <ul id=\"languageList\">\n                           ").concat(languageArray.map(function (languageItem) { return "<li>".concat(languageItem, "</li>"); }), "\n                    </ul>\n                </div>\n            </div>\n        </div>\n    </div>\n    </div>\n    <div class=\"lowerBtns\">\n    <button class=\"lowerBtns\" onclick=\"editResume('").concat(userName.value, "','").concat(jobTitle.value, "','").concat(email.value, "','").concat(phoneNum.value, "','").concat(profileBio, "',").concat(educationArray, ",").concat(skillsArray, ",").concat(experienceArray, ",").concat(languageArray, ")\">Edit Resume</button>\n    <button class=\"lowerBtns\" onclick=\"downloadPdf()\">Download pdf</button>\n    <button class=\"lowerBtns\"><a href=\"").concat(generatedLink, "\" target=\"_blank\">Share link</a></button>\n    </div>\n    ");
     }
+};
+var editResume = function (name, jobTitle, email, phoneNum, profileBio, educationArray, skillsArray, experienceArray, languageArray) {
+    resumeDiv.innerHTML = "           \n            <p id=\"createResumeHeading\">Update Resume</p>\n            <form method=\"post\" onsubmit=\"updateResume(event)\">\n                <div class=\"inputDiv\">\n                    <input value=\"".concat(name, "\" id=\"name\" type=\"name\" placeholder=\"Your Name\">\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"jobTitle\" type=\"text\" placeholder=\"Your job title\">\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"email\" type=\"email\" placeholder=\"Your Email\">\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"phoneNum\" type=\"number\" placeholder=\"Phone No\">\n                </div>\n                <div class=\"inputDiv\">\n                    <textarea id=\"profileBio\" placeholder=\"Enter profile bio\" maxlength=\"400\"></textarea>\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"educationInput\" type=\"text\" placeholder=\"Add Education\">\n                    <button class=\"addBtn\" onclick=\"addEducation()\">+</button>\n                    <ul id=\"educationList\"></ul>\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"experienceInput\" type=\"text\" placeholder=\"Add Experience\">\n                    <button class=\"addBtn\" onclick=\"addExperience()\">+</button>\n                    <ul id=\"experienceList\"></ul>\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"skillsInput\" type=\"text\" placeholder=\"Add Skills\">\n                    <button class=\"addBtn\" onclick=\"addSkill()\">+</button>\n                    <ul id=\"skillsList\"></ul>\n                </div>\n                <div class=\"inputDiv\">\n                    <input id=\"languageInput\" type=\"text\" placeholder=\"Add Language\">\n                    <button class=\"addBtn\" onclick=\"addLanguage()\">+</button>\n                    <ul id=\"languageList\">\n").concat(languageArray.map(function (item) { return "<li>".concat(item, "</li>"); }), "\n                    </ul>\n                </div>\n                <div class=\"inputDiv\">\n                    <img id=\"fileImg\" src=\"\" alt=\"\">\n                    <input type=\"file\" id=\"fileInput\" onchange=\"uploadProfilePic()\" />\n                </div>\n                <button>Update Resume</button>\n                ");
 };
 var uploadProfilePic = function () {
     var fileInput = document.getElementById("fileInput");
@@ -118,9 +148,3 @@ var uploadProfilePic = function () {
         fileImg.src = URL.createObjectURL(fileInput.files[0]);
     }
 };
-// let profileBtn = document.getElementById("profileDiv") as HTMLDivElement;
-// profileBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   let fileInput = document.getElementById("fileInput") as HTMLInputElement;
-//   fileInput.click();
-// });
--- resume-builder.ts
+++ resume-builder.ts
@@ -1,11 +1,12 @@
 let userName = document.getElementById("name") as HTMLInputElement;
 let email = document.getElementById("email") as HTMLInputElement;
-let jobTitle = document.getElementById("email") as HTMLInputElement;
+let jobTitle = document.getElementById("jobTitle") as HTMLInputElement;
 let profileBio = document.getElementById("profileBio") as HTMLInputElement;
 let phoneNum = document.getElementById("phoneNum") as HTMLInputElement;
 let skill = document.getElementById("skillsInput") as HTMLInputElement;
 let education = document.getElementById("educationInput") as HTMLInputElement;
 let experience = document.getElementById("experienceInput") as HTMLInputElement;
+let language = document.getElementById("languageInput") as HTMLInputElement;
 let resumeDiv = document.getElementById("resumeDiv") as HTMLDivElement;
 let alertDiv = document.getElementById("alert") as HTMLDivElement;
 let errorMessage = document.getElementById("error_message") as HTMLSpanElement;
@@ -16,11 +17,14 @@ let skillsList = document.getElementById("skillsList") as HTMLUListElement;
 let experienceList = document.getElementById(
   "experienceList"
 ) as HTMLUListElement;
+let languageList = document.getElementById("languageList") as HTMLUListElement;
+
 let selectedFile: string;
 
 let educationArray: string[] = [];
 let skillsArray: string[] = [];
 let experienceArray: string[] = [];
+let languageArray: string[] = [];
 
 const closeErrorAlert = () => {
   alertDiv.style.opacity = "0";
@@ -33,7 +37,6 @@ const closeErrorAlert = () => {
 const addEducation = () => {
   if (education.value.trim().length < 1) {
     education.focus();
-    return;
   } else {
     educationArray.unshift(education.value);
     educationList.innerHTML += `<li>${education.value}</li>`;
@@ -43,7 +46,6 @@ const addEducation = () => {
 const addSkill = () => {
   if (skill.value.trim().length < 1) {
     skill.focus();
-    return;
   } else {
     skillsArray.unshift(skill.value);
     skillsList.innerHTML += `<li>${skill.value}</li>`;
@@ -53,11 +55,19 @@ const addSkill = () => {
 const addExperience = () => {
   if (experience.value.trim().length < 1) {
     experience.focus();
-    return;
   } else {
     experienceArray.unshift(experience.value);
     experienceList.innerHTML += `<li>${experience.value}</li>`;
     experience.value = "";
+  }
+};
+const addLanguage = () => {
+  if (language.value.trim().length < 1) {
+    language.focus();
+  } else {
+    languageArray.unshift(language.value);
+    languageList.innerHTML += `<li>${language.value}</li>`;
+    language.value = "";
   }
 };
 
@@ -70,34 +80,52 @@ interface Resume {
   skills: string[];
   education: string[];
   experience: string[];
+  languages: string[];
   profileImg: string;
 }
-const createResume = (event: any) => {
+
+const createResume = (event) => {
   event.preventDefault();
   if (userName.value.trim().length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your name";
+    userName.focus();
   } else if (jobTitle.value.trim().length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your job title";
+    jobTitle.focus();
   } else if (email.value.trim().length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your email";
+    email.focus();
   } else if (phoneNum.value.trim().length < 0) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your phone number";
+    phoneNum.focus();
   } else if (profileBio.value.trim().length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your profile bio";
+    profileBio.focus();
   } else if (educationArray.length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please add atleast your last education";
+    education.focus();
   } else if (experienceArray.length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please enter your last experience";
+    experience.focus();
   } else if (skillsArray.length < 1) {
     alertDiv.style.display = "block";
     errorMessage.innerText = "Please add atleast one skill";
+    skill.focus();
+  } else if (languageArray.length < 1) {
+    alertDiv.style.display = "block";
+    errorMessage.innerText = "Please add atleast one language";
+    language.focus();
+  } else if (!selectedFile) {
+    alertDiv.style.display = "block";
+    errorMessage.innerText = "Please upload your profile picture";
+    language.focus();
   } else {
     let resume: Resume = {
       name: userName.value,
@@ -108,20 +136,143 @@ const createResume = (event: any) => {
       skills: skillsArray,
       education: educationArray,
       experience: experienceArray,
+      languages: languageArray,
       profileImg: selectedFile,
     };
     localStorage.setItem("resume", JSON.stringify(resume));
 
-    resumeDiv.innerHTML = `<div>
-      <h1>Your name: ${userName.value}</h1>
-      <h1>Your email: ${email.value}</h1>
-      <h1>Your phone number: ${phoneNum.value}</h1>
-      <img src="${selectedFile}" height="100" width="100" />
-      </div>`;
-    userName.value = "";
-    email.value = "";
-    phoneNum.value = "";
+    let generatedLink = `https://dynamic-resume-generator-fg4kq6y7p-ghulamqadir1s-projects.vercel.app/${userName.value}`;
+    resumeDiv.innerHTML = `<div id="myResumeDiv">
+        <div class="headerDiv">
+            <div class="leftHandHeader">
+                <div>
+                    <p class="name">${userName.value}</p>
+                    <p class="jobTitle">${jobTitle.value}</p>
+                </div>
+                <img class="profileImg" src="${selectedFile}" />
+            </div>
+            <div class="rightHandHeader">
+                <p class="email">Email: ${email.value}</p>
+                <p class="phoneNo">Phone No: ${phoneNum.value}</p>
+            </div>
+        </div>
+
+           <div class="mainContentDiv">
+            <div class="leftHandMainDiv">
+                <div class="aboutMeDiv mainDivs">
+                    <p class="mainDivHeadings">About Me</p>
+                    <p class="aboutMeContent">${profileBio.value}</p>
+                </div>
+                <div class="educationDiv mainDivs">
+                    <p class="mainDivHeadings">Education</p>
+                    <ul id="educationList">
+            ${educationArray.map(
+              (educationItem) => `<li>${educationItem}</li>`
+            )}
+                    </ul>
+                </div>
+                <div class="experienceDiv mainDivs">
+                    <p class="mainDivHeadings">Experience</p>
+                  
+                    <ul id="experienceList">
+                            ${experienceArray.map(
+                              (expItem) => `<li>${expItem}</li>`
+                            )}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="rightHandMainDiv">
+                <div class="experienceDiv mainDivs">
+                    <p class="mainDivHeadings">Skills</p>
+                    <ul id="skillsList">
+                             ${skillsArray.map(
+                               (skillItem) => `<li>${skillItem}</li>`
+                             )}
+                    </ul>
+                </div>
+                <div class="languageDiv">
+                    <p class="mainDivHeadings mainDivs">Languages</p>
+                    <ul id="languageList">
+                           ${languageArray.map(
+                             (languageItem) => `<li>${languageItem}</li>`
+                           )}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="lowerBtns">
+    <button class="lowerBtns" onclick="editResume('${userName.value}','${
+      jobTitle.value
+    }','${email.value}','${
+      phoneNum.value
+    }','${profileBio}',${educationArray},${skillsArray},${experienceArray},${languageArray})">Edit Resume</button>
+    <button class="lowerBtns" onclick="downloadPdf()">Download pdf</button>
+    <button class="lowerBtns"><a href="${generatedLink}" target="_blank">Share link</a></button>
+    </div>
+    `;
   }
+};
+
+const editResume = (
+  name: string,
+  jobTitle: string,
+  email: string,
+  phoneNum: string,
+  profileBio: string,
+  educationArray: string[],
+  skillsArray: string[],
+  experienceArray: string[],
+  languageArray: string[]
+) => {
+  resumeDiv.innerHTML = `           
+            <p id="createResumeHeading">Update Resume</p>
+            <form method="post" onsubmit="updateResume(event)">
+                <div class="inputDiv">
+                    <input value="${name}" id="name" type="name" placeholder="Your Name">
+                </div>
+                <div class="inputDiv">
+                    <input id="jobTitle" type="text" placeholder="Your job title">
+                </div>
+                <div class="inputDiv">
+                    <input id="email" type="email" placeholder="Your Email">
+                </div>
+                <div class="inputDiv">
+                    <input id="phoneNum" type="number" placeholder="Phone No">
+                </div>
+                <div class="inputDiv">
+                    <textarea id="profileBio" placeholder="Enter profile bio" maxlength="400"></textarea>
+                </div>
+                <div class="inputDiv">
+                    <input id="educationInput" type="text" placeholder="Add Education">
+                    <button class="addBtn" onclick="addEducation()">+</button>
+                    <ul id="educationList"></ul>
+                </div>
+                <div class="inputDiv">
+                    <input id="experienceInput" type="text" placeholder="Add Experience">
+                    <button class="addBtn" onclick="addExperience()">+</button>
+                    <ul id="experienceList"></ul>
+                </div>
+                <div class="inputDiv">
+                    <input id="skillsInput" type="text" placeholder="Add Skills">
+                    <button class="addBtn" onclick="addSkill()">+</button>
+                    <ul id="skillsList"></ul>
+                </div>
+                <div class="inputDiv">
+                    <input id="languageInput" type="text" placeholder="Add Language">
+                    <button class="addBtn" onclick="addLanguage()">+</button>
+                    <ul id="languageList">
+${languageArray.map((item) => `<li>${item}</li>`)}
+                    </ul>
+                </div>
+                <div class="inputDiv">
+                    <img id="fileImg" src="" alt="">
+                    <input type="file" id="fileInput" onchange="uploadProfilePic()" />
+                </div>
+                <button>Update Resume</button>
+                `;
 };
 
 const uploadProfilePic = () => {
@@ -133,10 +284,3 @@ const uploadProfilePic = () => {
     fileImg.src = URL.createObjectURL(fileInput.files[0]);
   }
 };
-
-// let profileBtn = document.getElementById("profileDiv") as HTMLDivElement;
-// profileBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   let fileInput = document.getElementById("fileInput") as HTMLInputElement;
-//   fileInput.click();
-// });
